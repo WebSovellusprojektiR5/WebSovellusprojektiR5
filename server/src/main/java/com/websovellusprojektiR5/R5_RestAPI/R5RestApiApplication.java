ce787ff099e87b163e5d33e03cb25cd66ab631b1
Mikko Turpeinen <t0tumi01@students.oamk.fi>
2022-03-21 10:07:59 +0200

    spring init done

---
+++ server/src/main/java/com/websovellusprojektiR5/R5_RestAPI/R5RestApiApplication.java
@@ -0,0 +1,13 @@
+package com.websovellusprojektiR5.R5_RestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R5RestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R5RestApiApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/websovellusprojektiR5/R5_RestAPI/R5RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websovellusprojektiR5.R5_RestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R5RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
