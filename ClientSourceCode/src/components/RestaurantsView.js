8782365bbb16220acdea6b9866d08807c96ce716
Mikko Turpeinen <t0tumi01@students.oamk.fi>
2022-04-04 19:43:04 +0300

    fixed current temporary frontend

--- ClientSourceCode/src/components/RestaurantsView.js
+++ ClientSourceCode/src/components/RestaurantsView.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function RestaurantsView(props) {
     return (
-    <div key={props.id} className="pageItem">
+    <div className="pageItem">
         <img src={props.item.thumbnail_url} />
         <div>{props.item.name}</div>
         <div>{props.item.description}</div>
